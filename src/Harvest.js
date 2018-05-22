f6687ad54b29875d7a53542d150720cdc8244bd0
nakata <github@satoyan.net>
2018-05-22 16:13:35 +0900

    :construct: Specify all files for api

--- src/Harvest.js
+++ src/Harvest.js
@@ -5,6 +5,31 @@ let HeaderAuth = require('./authentication/HeaderAuth'),
     path = require('path'),
     camelCase = require('lodash/camelCase');
 
+const apiFiles = [
+    "Client_Contacts",
+    "Clients",
+    "Company",
+    "Estimate_Item_Categories",
+    "Estimate_Line_Item",
+    "Estimate_Messages",
+    "Estimates",
+    "Expense_Categories",
+    "Expenses",
+    "Invoice_Item_Categories",
+    "Invoice_Line_Item",
+    "Invoice_Messages",
+    "Invoice_Payments",
+    "Invoices",
+    "Projects",
+    "Project_Task_Assignments",
+    "Project_User_Assignments",
+    "Roles",
+    "Tasks",
+    "Time_Entries",
+    "User_Project_Assignments",
+    "Users",
+];
+
 module.exports = class Harvest {
     constructor(config) {
         this.headerAuth = new HeaderAuth(config);
@@ -17,12 +42,18 @@ module.exports = class Harvest {
             resolveWithFullResponse: false
         };
 
-        fs.readdirSync(path.join(__dirname, 'api')).forEach(name => {
-            // Refacto this line
-            let prop = camelCase(name.slice(0, -3));
+        apiFiles.forEach(name => {
+            let prop = camelCase(name);
             let Resource = require(`./api/${name}`);
-
             this[prop] = new(Resource)(this.options);
-        });
+        })
+
+        // fs.readdirSync(path.join(__dirname, 'api')).forEach(name => {
+        //     // Refacto this line
+        //     let prop = camelCase(name.slice(0, -3));
+        //     let Resource = require(`./api/${name}`);
+
+        //     this[prop] = new(Resource)(this.options);
+        // });
     }
 };
